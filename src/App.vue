d61a798580bea249da8ebb6586147835fada0835
Oppiym <oppiym@gmail.com>
2019-11-13 10:30:34 +0300

    Logic

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
-    <v-layout row wrap>
+    <v-layout>
+        <v-flex v-for= "venue in venues" :key="venue">
+            <h3>{{venue}}</h3>
+            <v-card
+            v-for="show in filteredShows" :key="show">
+                <v-card-title>{{show}}</v-card-title>
+            </v-card>
+        </v-flex>
 
-{{GetAllVenueTitles}}
 
 
 
-  <v-card v-for="event in events" :key="event"
+
+<!--
+  <v-card v-for="(event, i) in events" :key="i"
     class="mx-auto"
     max-width="400"
   >
@@ -17,12 +25,14 @@
       <v-card-title>{{event.event_title}}</v-card-title>
     </v-img>
 
-    <v-card-subtitle class="pb-0">{{event.show_id}}</v-card-subtitle>
+    <v-card-subtitle class="pb-0">{{event.venue_title}}</v-card-subtitle>
 
     <v-card-text class="text--primary">
-      <div>Whitehaven Beach</div>
+      <div>{{event.show_start}}</div>
 
-      <div>Whitsunday Island, Whitsunday Islands</div>
+      <div>{{event.hall_title}}</div>
+      <div>{{event.hall_title}}</div>
+
     </v-card-text>
 
     <v-card-actions>
@@ -30,18 +40,11 @@
         color="orange"
         text
       >
-        Share
-      </v-btn>
-
-      <v-btn
-        color="orange"
-        text
-      >
-        Explore
+        Купить билет
       </v-btn>
     </v-card-actions>
   </v-card>
-
+-->
 </v-layout>
 </template>
 
@@ -58,33 +61,43 @@ export default {
   data: () => ({
     events: [],
     errors: [],
-    venues: []
-
-  }),
+    venues:[],
+    venue_titles_formatted: []
+     }),
   created() {
       HTTP
-      .get ('')
+      .get ('shows/0/0/0/0')
       .then(response => {
           this.events = response.data
-          console.log(this.events)
-         })
+
+          var i
+          var venue_titles = []
+          var venue_titles_formatted = []
+//Создаем новый массив со всеми названиями площадок
+          for (i=0; i < this.events.length;i++){
+          venue_titles.push(this.events[i].venue_title)
+            }
+//Оставляем только уникальные номера площадок
+        venue_titles_formatted = Array.from(new Set(venue_titles))
+
+//Необходимая площадка на первом месте
+        var j = 0
+        var b = 1
+        venue_titles_formatted[j] = venue_titles_formatted.splice(b,1,venue_titles_formatted[j])[0]
+        console.log(venue_titles_formatted, typeof venue_titles_formatted)
+        this.venues = venue_titles_formatted
+        console.log(this.venues, typeof this.venues)
+    }
+)
       .catch (e=>{
           this.errors.push(e)
       })
   },
-
-  computed:{
-      GetAllVenueTitles: (events)=> {
-        for (var i = 0; i< events.length; i++){
-            return  alert("Привет!")
-
-          }
-      }
-
-  }
-
-
-
+  computed: {
+    filteredShows() {
+        var
+        return
+    }
 
 
 
--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const HTTP = axios.create({
-    baseURL: 'https://api.intickets.ru/widget/v1/events/',
+    baseURL: 'https://api.intickets.ru/widget/v1/',
     timeout: 1000,
     headers: {
     Authorization: 'Bearer 7c23494f-03c8-4b6f-ceda-e18635145b5b'}
